26f7782be905aa490d0d39edb5cccb9bb67c34ab
Glitch (general-bramble-shock) <none>
2020-12-13 12:29:14 +0000

    🍎🌍 Checkpoint ./server.js:23347598/396

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,6 +18,11 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/whoami", function (req, res) {
+  const language = req.header('Accept-Languge');
+  const dd = req.header('User-Agent');
+  res.json({ langauge});
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
